cdd8e07631f6d3fccbf509c8b443fa2cc9cf67e5
lhk3337 <lhk3337@naver.com>
2020-06-24 17:50:18 +0900

    add filter method and foreach method

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const jsTag1 = document.querySelector(".tag1");
-const jsTag2 = document.querySelector(".tag2");
-const jsTag3 = document.querySelector(".tag3");
-const jsTag4 = document.querySelector(".tag4");
-const jsTag5 = document.querySelector(".tag5");
-const jsTag6 = document.querySelector(".tag6");
-const jsTag7 = document.querySelector(".tag7");
-const jsTag8 = document.querySelector(".tag8");
-const jsTag9 = document.querySelector(".tag9");
-
 const jsListT = document.querySelector(".listT");
 const jsListP = document.querySelector(".listP");
 const jsListS = document.querySelector(".listS");
@@ -15,38 +5,161 @@ const jsListS = document.querySelector(".listS");
 const jslistBlue = document.querySelector(".blue");
 const jsListYellow = document.querySelector(".yellow");
 const jsListPink = document.querySelector(".pink");
-
+const jsitems = document.querySelector("#items");
 const url =
   "https://raw.githubusercontent.com/lhk3337/clothingshop/master/cloth.json";
 fetch(url)
   .then((res) => res.json())
   .then((json) => {
-    jsTag1.innerHTML = [json[0].title, json[0].gender, json[0].color].join(
-      ", "
-    );
-    jsTag2.innerHTML = [json[1].title, json[1].gender, json[1].color].join(
-      ", "
-    );
-    jsTag3.innerHTML = [json[2].title, json[2].gender, json[2].color].join(
-      ", "
-    );
-    jsTag4.innerHTML = [json[3].title, json[3].gender, json[3].color].join(
-      ", "
-    );
-    jsTag5.innerHTML = [json[4].title, json[4].gender, json[4].color].join(
-      ", "
-    );
-    jsTag6.innerHTML = [json[5].title, json[5].gender, json[5].color].join(
-      ", "
-    );
-    jsTag7.innerHTML = [json[6].title, json[6].gender, json[6].color].join(
-      ", "
-    );
-    jsTag8.innerHTML = [json[7].title, json[7].gender, json[7].color].join(
-      ", "
-    );
-    jsTag9.innerHTML = [json[8].title, json[8].gender, json[8].color].join(
-      ", "
-    );
+    json.forEach((element) => {
+      jsli = document.createElement("li");
+      jsitems.append(jsli);
+      let img = document.createElement("img");
+      img.src = element.picture;
+      jsli.append(img);
+      let jsSpan = document.createElement("span");
+      jsSpan.innerHTML = [
+        element.title,
+        element.gender,
+        element.size,
+        element.color,
+      ].join(", ");
+      jsli.append(jsSpan);
+    });
+
+    jsListT.addEventListener("click", () => {
+      let result = json.filter((x) => {
+        return x.title === "tshirt";
+      });
+      result.forEach((element) => {
+        jsli1 = document.createElement("li");
+        jsitems.append(jsli1);
+        let img1 = document.createElement("img");
+        img1.src = element.picture;
+        jsli1.append(img1);
+        let jsSpan1 = document.createElement("span");
+        jsSpan1.innerHTML = [
+          element.title,
+          element.gender,
+          element.size,
+          element.color,
+        ].join(", ");
+        jsli1.append(jsSpan1);
+      });
+      console.log("TSHIRT");
+    });
+
+    jsListP.addEventListener("click", () => {
+      let result = json.filter((x) => {
+        return x.title === "pants";
+      });
+      result.forEach((element) => {
+        jsli1 = document.createElement("li");
+        jsitems.append(jsli1);
+        let img1 = document.createElement("img");
+        img1.src = element.picture;
+        jsli1.append(img1);
+        let jsSpan1 = document.createElement("span");
+        jsSpan1.innerHTML = [
+          element.title,
+          element.gender,
+          element.size,
+          element.color,
+        ].join(", ");
+        jsli1.append(jsSpan1);
+      });
+      console.log(result);
+      console.log("Pants");
+    });
+    jsListS.addEventListener("click", () => {
+      let result = json.filter((x) => {
+        return x.title === "skirt";
+      });
+      result.forEach((element) => {
+        jsli1 = document.createElement("li");
+        jsitems.append(jsli1);
+        let img1 = document.createElement("img");
+        img1.src = element.picture;
+        jsli1.append(img1);
+        let jsSpan1 = document.createElement("span");
+        jsSpan1.innerHTML = [
+          element.title,
+          element.gender,
+          element.size,
+          element.color,
+        ].join(", ");
+        jsli1.append(jsSpan1);
+      });
+      console.log(result);
+      console.log("Skirt");
+    });
+
+    jslistBlue.addEventListener("click", () => {
+      let result = json.filter((x) => {
+        return x.color === "blue";
+      });
+      result.forEach((element) => {
+        jsli1 = document.createElement("li");
+        jsitems.append(jsli1);
+        let img1 = document.createElement("img");
+        img1.src = element.picture;
+        jsli1.append(img1);
+        let jsSpan1 = document.createElement("span");
+        jsSpan1.innerHTML = [
+          element.title,
+          element.gender,
+          element.size,
+          element.color,
+        ].join(", ");
+        jsli1.append(jsSpan1);
+      });
+      console.log(result);
+      console.log("Blue");
+    });
+    jsListYellow.addEventListener("click", () => {
+      let result = json.filter((x) => {
+        return x.color === "yellow";
+      });
+      result.forEach((element) => {
+        jsli1 = document.createElement("li");
+        jsitems.append(jsli1);
+        let img1 = document.createElement("img");
+        img1.src = element.picture;
+        jsli1.append(img1);
+        let jsSpan1 = document.createElement("span");
+        jsSpan1.innerHTML = [
+          element.title,
+          element.gender,
+          element.size,
+          element.color,
+        ].join(", ");
+        jsli1.append(jsSpan1);
+      });
+      console.log(result);
+      console.log("Yellow");
+    });
+    jsListPink.addEventListener("click", () => {
+      let result = json.filter((x) => {
+        return x.color === "pink";
+      });
+      result.forEach((element) => {
+        jsli1 = document.createElement("li");
+        jsitems.append(jsli1);
+        let img1 = document.createElement("img");
+        img1.src = element.picture;
+        jsli1.append(img1);
+        let jsSpan1 = document.createElement("span");
+        jsSpan1.innerHTML = [
+          element.title,
+          element.gender,
+          element.size,
+          element.color,
+        ].join(", ");
+        jsli1.append(jsSpan1);
+      });
+      console.log(result);
+      console.log("Pink");
+    });
   })
+
   .catch((err) => console.log(err));
